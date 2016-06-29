a03dc2ed235c620f54846a92f72685e5b48d0bc2
Cristian Oliveira <contato@cristianoliveira.com.br>
2016-06-29 20:01:46 +0530

    feat: call for help

--- test/boteam.js
+++ test/boteam.js
@@ -24,65 +24,106 @@ describe('When adding member to team', function() {
   });
 
   describe('member mentioned', function () {
-    it('should add to team', function () {
+    it('should add to the team', function () {
       var slackId= "<@U1NCN9DAS>",
         channel= "channel_id",
         teamMemberSpy;
 
-      repository.teamMember.save = sinon.spy();
+      repository.teamMember.add = sinon.spy();
 
       message.text =  "add " + slackId;
       message.channel = channel;
 
       boteam.onAdd(bot, message);
 
-      assert.isTrue(repository.teamMember.save.calledOnce);
-      repository.teamMember.save.calledWith({
+      assert.isTrue(repository.teamMember.add.calledOnce);
+      repository.teamMember.add.calledWith({
         channel: channel,
         slackId: slackId
       });
     });
 
-    it('should add more than one to team', function () {
+    it('should add more than one to the team', function () {
       var slackId1 = "<@U1NCN9DAS>",
         slackId2 = "<@U1NCN9QWE>",
         channel= "channel_id",
         teamMemberSpy;
 
-      repository.teamMember.save = sinon.spy();
+      repository.teamMember.add = sinon.spy();
 
       message.text =  "add " + slackId1 + " and " + slackId2;
       message.channel = channel;
 
       boteam.onAdd(bot, message);
 
-      assert.isTrue(repository.teamMember.save.calledTwice);
-      repository.teamMember.save.calledWith({
+      assert.isTrue(repository.teamMember.add.calledTwice);
+      repository.teamMember.add.calledWith({
         channel: channel,
         slackId: slackId1
       });
-      repository.teamMember.save.calledWith({
+      repository.teamMember.add.calledWith({
         channel: channel,
         slackId: slackId2
       });
     });
   });
+});
 
-  describe('When requesting help', function() {
-    beforeEach(function() {
-      message = {};
-      bot.reply = sinon.spy();
-      boteam = Boteam(repository);
+describe('When requesting help', function() {
+  beforeEach(function() {
+    message = {};
+    bot.reply = sinon.spy();
+    boteam = Boteam(repository);
+  });
+
+  describe("empty team", function(){
+    it("should say there is no one to help", function() {
+      message.text = "SOS";
+
+      boteam.onHelp(bot, message);
+
+      bot.reply.calledWith(message, "No one can help you, sorry. :/");
     });
+  });
 
-    describe("empty team", function(){
-      it("should say there is no one to help", function() {
-        message.text = "SOS";
+  describe("has error", function(){
+    it("should say was not removed", function() {
+      var channel = "CHANELID",
+        members = [],
+        error = "some cause";
 
-        boteam.onHelp(bot, message);
+      message.text = "SOS";
+      message.channel = channel;
 
-        bot.reply.calledWith(message, "No one can help you, sorry. :/");
-      });
+      repository.teamMember.fromChannel = sinon.stub()
+      repository.teamMember.fromChannel.yields(error, null);
+
+      boteam.onHelp(bot, message);
+
+      bot.reply.calledWith(message,
+                           "Sorry was not possible remove. Cause: " + error);
+    });
+  });
+
+  describe("has team", function(){
+    it("should mention the member", function() {
+      var channel = "CHANELID",
+        member1 = {},
+        members = [];
+
+      member1 = { slackId: "xpto" };
+      members.push(member1);
+
+      message.text = "SOS";
+      message.channel = channel;
+
+      repository.teamMember.fromChannel = sinon.stub()
+      repository.teamMember.fromChannel.yields(null, members);
+
+      boteam.onHelp(bot, message);
+
+      bot.reply.calledWith(message,
+                           "Guys please help here. Team: " + member1.slackId);
     });
   });
 });
