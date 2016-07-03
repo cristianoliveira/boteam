21b52c4d86b8f1710ee95bb9febb6c01328b6aa4
Cristian Oliveira <contato@cristianoliveira.com.br>
2016-07-03 17:16:57 -0300

    chore: adding watch task

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,26 @@
-var gulp = require('gulp');
-var mocha = require('gulp-mocha');
-var jshint = require('gulp-jshint');
+var gulp = require('gulp'),
+  mocha = require('gulp-mocha'),
+  jshint = require('gulp-jshint'),
+  watch = require('gulp-watch'),
+  paths = {
+    lib: './lib/*.js',
+    tests: './tests/*.js'
+  };
+
+gulp.task('watch', function () {
+  gulp.watch(paths.lib, ['default']);
+  gulp.watch(paths.tests, ['tests']);
+});
 
 gulp.task('test', function(){
-  return gulp.src('test/*.js', {read: false}).pipe(mocha({reporter: 'nyan'}));
+  return gulp.src(paths.tests, {read: false}).pipe(mocha({reporter: 'nyan'}));
 });
 
 gulp.task('lint', function() {
-  return gulp.src('./lib/*.js')
+  return gulp.src(paths.lib)
     .pipe(jshint())
-    .pipe(jshint.reporter('default'));
+    .pipe(jshint.reporter('default'))
+    .pipe(jshint.reporter('fail'));
 });
 
 gulp.task('default', ['lint', 'test']);
